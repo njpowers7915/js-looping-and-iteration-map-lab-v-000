4855daef104cc35be6a84bc57e14f6820156cbfa
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 17:46:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(function(i) {return i.toLowerCase();});
 }
 
-function nameToAttributes() {
+function nameToAttributes(drivers) {
   return drivers.map(function(i) {return i.split(" ");});
 }
 
