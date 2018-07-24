ea0c63d799d504844a2147ee150968a9c0589715
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 17:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(i) {return i.toLowerCase();});
+}
+
+function nameToAttributes() {
+
+}
+
+function attributesToPhrase() {
+
+}
